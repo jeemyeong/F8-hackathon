ae14e766401518d2105e0ae55cab21dd233917e8
Leo.fp <Leo.fp@kakaocorp.com>
2019-04-30 08:30:28 -0700

    Update

--- bot/src/app.ts
+++ bot/src/app.ts
@@ -1,15 +1,15 @@
 import * as AWS from "aws-sdk";
-import { BotServer, BotUtils, Webhook } from './@aiteq/messenger-bot';
-import { format } from "date-fns";
-import { surveyHandler } from "./handler/surveyHandler";
-
 require('dotenv').config();
-
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: 'us-west-2'
 });
+
+import { BotServer, BotUtils, Webhook } from './@aiteq/messenger-bot';
+import { format } from "date-fns";
+import { surveyHandler } from "./handler/surveyHandler";
+
 
 
 let bot = new BotServer({
@@ -29,6 +29,7 @@ bot.on(Webhook.Event.PERSISTENT_MENU, "get-started", surveyHandler);
 bot.on(Webhook.Event.GET_STARTED_BUTTON, "get-started", surveyHandler);
 bot.on(Webhook.Event.TEXT_MESSAGE, surveyHandler);
 bot.on(Webhook.Event.TEXT_QUICK_REPLY, "from_ad", surveyHandler);
+bot.on(Webhook.Event.POSTBACK, "from_ad", surveyHandler);
 
 // @ts-ignore
 const app = bot.app;
